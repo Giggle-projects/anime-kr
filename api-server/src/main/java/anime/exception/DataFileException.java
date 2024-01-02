ce4a7d919f66d148d01d20f7a44ea4b72af44eee
ecsimsw <ecsimsw@gmail.com>
2024-01-03 08:34:12 +0900

    PR의 변경 사항 폴더 검증

--- api-server/src/main/java/anime/exception/DataFileException.java
+++ api-server/src/main/java/anime/exception/DataFileException.java
@@ -4,13 +4,5 @@ public class DataFileException extends IllegalArgumentException {
 
     public DataFileException(String message, Throwable cause) {
         super(message, cause);
-
-
-
-
-
-
-
-
     }
 }
