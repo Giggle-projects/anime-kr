3ee2b6fe917deb2bc98bb165483a1e652582af7b
ecsimsw <ecsimsw@gmail.com>
2024-01-04 05:17:01 +0900

    서비스 포트 변경

--- infra-k8s/kube-portforward.sh
+++ infra-k8s/kube-portforward.sh
@@ -2,4 +2,4 @@
 
 # kong ingress proxy
 nohup kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address 0.0.0.0 &
-nohup kubectl port-forward -n animekr service/animekr-svc 11013:80 --address 0.0.0.0 &
+nohup kubectl port-forward -n animekr service/animekr-svc 18080:80 --address 0.0.0.0 &
