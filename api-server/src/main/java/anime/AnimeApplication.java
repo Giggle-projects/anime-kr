b5416aa89ac6672dfa5c2b71db9153544b1bf592
ecsimsw <ecsimsw@gmail.com>
2024-01-05 06:46:22 +0900

    모니터링 설정

--- api-server/src/main/java/anime/AnimeApplication.java
+++ api-server/src/main/java/anime/AnimeApplication.java
@@ -8,6 +8,7 @@ public class AnimeApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(AnimeApplication.class);
+        app.setAdditionalProfiles("prod");
         app.run(args);
     }
 }
---
+++ utils-monitoring/README.md
@@ -0,0 +1,8 @@
+## Monitoring
+
+### PORTS
+```
+cAdvisor   : 9091
+grafana    : 9092
+prometheus : 9093
+```
