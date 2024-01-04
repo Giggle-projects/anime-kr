5d538b71e07254b371982a7b49be58250c61c4ca
ecsimsw <ecsimsw@gmail.com>
2024-01-04 13:58:26 +0900

    게이트웨이 테스트

--- infra-k8s/kube-portforward.sh
+++ infra-k8s/kube-portforward.sh
@@ -4,3 +4,4 @@
 nohup kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address 0.0.0.0 &
 nohup kubectl port-forward -n animekr service/animekr-svc 18080:8080 --address 0.0.0.0 &
 #nohup kubectl port-forward -n animekr animekr-api-844bfbdd84-pdr54 18080:8080 --address 0.0.0.0 &
+nohup kubectl port-forward -n test test-nginx-deploy-658dc494b9-97xq7 18080:8080 --address 0.0.0.0 &
