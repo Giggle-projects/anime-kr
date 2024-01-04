8bc0d4161f833eeb75b579b228a5bddbdab813e9
ecsimsw <ecsimsw@gmail.com>
2024-01-04 16:10:04 +0900

    롤링 업데이트 배포 스크립트

--- infra-service/rolling-deploy.sh
+++ infra-service/rolling-deploy.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+# Perform a health check by waiting for the service to be healthy
+HEALTH_CHECK_INTERVAL=10  # interval seconds
+MAX_RETRIES=10            # Maximum number of retries
+
 # Set the service name to restart
-SERVICE_NAME="app1"
+SERVICE_NAME="anime-1"
+
+# Get health check endpoint of service
+URL=localhost
+PORT=$(docker port $SERVICE_NAME | cut -d: -f2)
+HEALTH_CHECK_ENDPOINT=${URL}:${PORT}/actuator/health
+echo "Restart $SERVICE_NAME, health check with $HEALTH_CHECK_ENDPOINT"
 
 # Restart the specified service
 docker-compose restart $SERVICE_NAME
 
-# Perform a health check by waiting for the service to be healthy
-HEALTH_CHECK_INTERVAL=10  # in seconds
-MAX_RETRIES=30  # Maximum number of retries
-
 retries=0
-
 while [ $retries -lt $MAX_RETRIES ]; do
-    HEALTH_STATUS=$(docker inspect --format='{{.State.Health.Status}}' "${SERVICE_NAME}")
-
-    if [ "$HEALTH_STATUS" = "healthy" ]; then
-        echo "Service $SERVICE_NAME is healthy!"
-        exit 0
+    is_container_running=$(docker inspect --format='{{.State.Running}}' "${SERVICE_NAME}")
+    api_health=$(curl -s ${HEALTH_CHECK_ENDPOINT} | jq -r '.status | tostring')
+    if [ "$is_container_running" = "true" && "$api_health" = "UP"]; then
+        echo "$SERVICE_NAME is healthy!"
+        break
     else
-        echo "Service $SERVICE_NAME is not healthy. Retrying in $HEALTH_CHECK_INTERVAL seconds..."
+        echo "$SERVICE_NAME is not healthy. Retrying in $HEALTH_CHECK_INTERVAL seconds..."
         sleep $HEALTH_CHECK_INTERVAL
         retries=$((retries + 1))
     fi
 done
 
-echo "Health check failed for service $SERVICE_NAME after $MAX_RETRIES retries."
-exit 1
+if [ ${retries} -ge ${MAX_RETRIES} ]
+    echo "Health check failed for service $SERVICE_NAME after $MAX_RETRIES retries."
+    echo "ERROR :: Deploy failed : $SERVICE_NAME"
+    exit 1
