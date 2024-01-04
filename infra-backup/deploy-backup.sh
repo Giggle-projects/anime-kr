349b3e65513599410a81b3b418e6685729ea7c9f
good0619@naver.com <32181771@dankook.ac.kr>
2024-01-05 07:42:06 +0900

    백업 서버 빌드 스크립트 작성

--- infra-backup/deploy-backup.sh
+++ infra-backup/deploy-backup.sh
@@ -2,13 +2,12 @@
 
 # Define Container name
 CONTAINER_NAME="anime-kr"
-IS_CONTAINER_PORT=docker ps -q --filter "publish=8080"
-IS_SAME_NAME_CONTAINER=$(docker ps --format '{{.Names}}' | grep 'anime-kr')
+container_ids=$(docker ps -q --filter "publish=8080")
 
 # container found with port 8080 exposed
-if [ [ -z "$IS_CONTAINER_PORT" ] ]; then
-  # No container found with name is  anime-kr
-  if [ ! [ docker ps --format '{{.Names}}' | grep '^anime-kr$' ] ]; then
+if [ ! -z $container_ids ]; then
+  # container found with name is anime-kr
+  if [[ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]]; then
     # cleanup
     docker rm $CONTAINER_NAME
   fi
@@ -17,7 +16,7 @@ fi
 # Check for any process using port 8080
 PID_8080=$(lsof -i :8080 -t)
 
-if [ ! [ -z "$pids" ] ]; then
+if [ ! -z "$PID_8080" ] ]; then
   for PID in PID_8080; do
     kill -9 PID
   done
