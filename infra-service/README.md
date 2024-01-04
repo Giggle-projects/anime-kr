a964673771aafd784c4d420f2a287c147562ac8f
ecsimsw <ecsimsw@gmail.com>
2024-01-04 16:55:02 +0900

    롤링 업데이트 배포 스크립트

--- infra-service/README.md
+++ infra-service/README.md
@@ -1,7 +1,36 @@
 ## Api bundle
 
+### Services
 ```
-18081:8080
-18082:8080
-18083:8080
+NAMES    IMAGE                                    PORTS
+anime-1  ghcr.io/giggle-projects/anime-kr:0.0.1   0.0.0.0:18081->8080/tcp
+anime-2  ghcr.io/giggle-projects/anime-kr:0.0.1   0.0.0.0:18082->8080/tcp
+anime-3  ghcr.io/giggle-projects/anime-kr:0.0.1   0.0.0.0:18083->8080/tcp
+```
+
+### Rolling update with health check
+```
+chmod +x deploy.sh
+./deploy.sh
+```
+
+### Deployment output
+```
+=== INFO :: Update anime-1 ===
+Restarting anime-1 ... done
+Health check with localhost:18081/actuator/health ...
+Health check with localhost:18081/actuator/health ...
+Service anime-1 is healthy!
+
+=== INFO :: Update anime-2 ===
+Restarting anime-2 ... done
+Health check with localhost:18082/actuator/health ...
+Health check with localhost:18082/actuator/health ...
+Service anime-2 is healthy!
+
+=== INFO :: Update anime-3 ===
+Restarting anime-3 ... done
+Health check with localhost:18083/actuator/health ...
+Health check with localhost:18083/actuator/health ...
+Service anime-3 is healthy!
 ```
---
+++ infra-service/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define service names
+SERVICE_NAMES=("anime-1" "anime-2" "anime-3")
+
+# Perform a health check by waiting for the service to be healthy
+HEALTH_CHECK_INTERVAL=1   # interval seconds
+MAX_RETRIES=10            # Maximum number of retries
+
+for SERVICE_NAME in "${SERVICE_NAMES[@]}"; do
+  echo "=== INFO :: Update $SERVICE_NAME ==="
+
+  # Get health check endpoint of service
+  URL=localhost
+  PORT=$(docker port $SERVICE_NAME | cut -d: -f2)
+  HEALTH_CHECK_ENDPOINT=${URL}:${PORT}/actuator/health
+
+  # Restart the specified service
+  docker-compose restart $SERVICE_NAME
+  sleep $HEALTH_CHECK_INTERVAL
+
+  # Check container status, API health with retry strategy
+  retries=0
+  while [ $retries -lt $MAX_RETRIES ]; do
+      echo "Health check with $HEALTH_CHECK_ENDPOINT ..."
+      is_container_running=$(docker inspect --format='{{.State.Running}}' "${SERVICE_NAME}")
+      api_health=$(curl -s ${HEALTH_CHECK_ENDPOINT} | jq -r '.status | tostring')
+      if [ "${is_container_running}" == "true" ] && [ "${api_health}" == "UP" ]; then
+          echo "Service $SERVICE_NAME is healthy!"
+          break
+      else
+          sleep $HEALTH_CHECK_INTERVAL
+          retries=$((retries + 1))
+      fi
+  done
+
+  # Throw exception when health check failed
+  if [ $retries -ge $MAX_RETRIES ]; then
+      echo "Health check failed for service $SERVICE_NAME after $MAX_RETRIES retries."
+      echo "=== ERROR :: Deploy failed : $SERVICE_NAME ==="
+      exit 1
+  fi
+
+  echo ""
+done
