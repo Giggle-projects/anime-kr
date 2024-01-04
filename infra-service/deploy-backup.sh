86e236e57cfbf807cf60a1c09610ca64bd102fe6
good0619@naver.com <32181771@dankook.ac.kr>
2024-01-05 06:40:40 +0900

    백업 서버 빌드 스크립트 작성

--- infra-service/deploy-backup.sh
+++ infra-service/deploy-backup.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Define Container anme
+# Define Container name
 CONTAINER_NAME="anime-kr"
+IS_CONTAINER_PORT=docker ps -q --filter "publish=8080"
+IS_SAME_NAME_CONTAINER=$(docker ps --format '{{.Names}}' | grep 'anime-kr')
 
-# Setting variable about sleep time & max retries value
-INITIAL_SLEEP=20
-MAX_RETRIES=10
-
-if [ ! "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
-  if [ "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
-  # cleanup
-  docker rm $CONTAINER_NAME
+# container found with port 8080 exposed
+if [ [ -z "$IS_CONTAINER_PORT" ] ]; then
+  # No container found with name is  anime-kr
+  if [ ! [ docker ps --format '{{.Names}}' | grep '^anime-kr$' ] ]; then
+    # cleanup
+    docker rm $CONTAINER_NAME
   fi
-  # run your container
-  retries=0
-  while [ $retries -lt $MAX_RETRIES ]; do
-    if [ ! "$(docker ps -a -q -f name=anime-kr)" ]; then
-      docker run -d --name anime-kr -p 8080:8080 ghcr.io/giggle-projects/anime-kr:latest
-      retries=$((retries + 1))
-      sleep $INITIAL_SLEEP
-    else
-      break
-    fi
+fi
+
+# Check for any process using port 8080
+PID_8080=$(lsof -i :8080 -t)
+
+if [ ! [ -z "$pids" ] ]; then
+  for PID in PID_8080; do
+    kill -9 PID
   done
 fi
+
+# build
+docker run -d --name anime-kr -p 8080:8080 ghcr.io/giggle-projects/anime-kr:latest
