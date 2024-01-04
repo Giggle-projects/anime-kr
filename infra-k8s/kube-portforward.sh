a3e08db0cfedb716079357eccae282729751cc55
ecsimsw <ecsimsw@gmail.com>
2024-01-04 15:07:46 +0900

    게이트웨이 백업 서버 추가

--- infra-k8s/kube-portforward.sh
+++
@@ -1,7 +0,0 @@
-# ps -ef | grep "kubectl port"
-
-# kong ingress proxy
-nohup kubectl port-forward -n kong service/kong-gateway-proxy 52080:80 --address 0.0.0.0 &
-nohup kubectl port-forward -n animekr service/animekr-svc 18080:8080 --address 0.0.0.0 &
-#nohup kubectl port-forward -n animekr animekr-api-844bfbdd84-pdr54 18080:8080 --address 0.0.0.0 &
-nohup kubectl port-forward -n test test-nginx-deploy-658dc494b9-97xq7 18080:8080 --address 0.0.0.0 &
