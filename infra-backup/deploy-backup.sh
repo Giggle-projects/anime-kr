5769e5ecbd8a3fdc6f5e8f74d9e9bddfbeb4e6b9
ecsimsw <ecsimsw@gmail.com>
2024-01-05 08:31:34 +0900

    JIB

--- infra-backup/deploy-backup.sh
+++ infra-backup/deploy-backup.sh
@@ -2,25 +2,13 @@
 
 # Define Container name
 CONTAINER_NAME="anime-kr"
-container_ids=$(docker ps -q --filter "publish=8080")
-
-# container found with port 8080 exposed
-if [ ! -z $container_ids ]; then
-  # container found with name is anime-kr
-  if [[ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]]; then
-    # cleanup
-    docker rm $CONTAINER_NAME
-  fi
+PORT=8080
+if [[ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]]; then
+  docker rm -f $CONTAINER_NAME
 fi
 
-# Check for any process using port 8080
-PID_8080=$(lsof -i :8080 -t)
+for PID in $(lsof -i :$PORT -t); do
+  kill -9 $PID
+done
 
-if [ ! -z "$PID_8080" ] ; then
-  for $PID in $PID_8080; do
-    kill -9 $PID
-  done
-fi
-
-# build
 docker run -d --name anime-kr -p 8080:8080 ghcr.io/giggle-projects/anime-kr:latest
