ac5753c278d8c8fbdbb41d7383170c6de34f8ce9
ecsimsw <ecsimsw@gmail.com>
2024-02-03 01:30:58 +0900

    remove test profile

--- application-server/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-# use animekr;
-#
-SELECT * FROM daily_count;
-
-INSERT INTO daily_count (id, today_count, total_count, yesterday_count, last_recorded)
-SELECT * FROM (SELECT 1, 0, 2, 3, NOW()) AS tmp
-WHERE NOT EXISTS (
-    SELECT id FROM daily_count WHERE id = 1
-) LIMIT 1;
